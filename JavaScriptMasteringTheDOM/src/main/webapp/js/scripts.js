8f5defba4a5f2500c98b51ab16b2c7139c811956
Nico Ziman <nziman@ausy-group.com>
2020-05-05 15:40:28 +0300

    remove script tags and js code

--- JavaScriptMasteringTheDOM/src/main/webapp/js/scripts.js
+++ JavaScriptMasteringTheDOM/src/main/webapp/js/scripts.js
@@ -1,4 +1,3 @@
-(function () {
 
     var carImages = document.querySelector(".carimages");
 
@@ -7,16 +6,6 @@
 
         var overlay = document.createElement("div");
         overlay.id = "overlay";
-
-        //set style
-        overlay.style.position = "absolute";
-        overlay.style.top = 0;
-        overlay.style.background = "rgba(0,0,0,0.7)";
-        overlay.style.cursor = "pointer";
-
-        //set sizes
-        overlay.style.width = window.innerWidth + "px";
-        overlay.style.height = window.innerHeight + "px";
 
         //get image source
         var imageSrc = e.target.src;
@@ -42,22 +31,6 @@
            }
         });
 
-        //set image to follow window scroll
-        window.addEventListener("scroll", function () {
-            if(overlay) {
-                overlay.style.top = window.pageYOffset + "px";
-                overlay.style.left = window.pageXOffset + "px";
-            }
-        });
-
-        //set overlay to resize when the window is resized
-        window.addEventListener("resize", function () {
-            if(overlay) {
-                overlay.style.width = window.innerWidth + "px";
-                overlay.style.height = window.innerHeight + "px";
-            }
-        });
-
         document.body.appendChild(overlay);
 
 
@@ -67,4 +40,3 @@
     }); //Enent listener for the div width class of carImage
 
     console.log("Works!")
-})(); //load the document first
